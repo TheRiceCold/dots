2bcdb8385b25352e486aa381fcfaa509154472bf
kaizen-dw <dalewaltergh@gmail.com>
2024-03-04 22:06:44 +0800

    feat: shell.nix for bootstrapping, and nix update

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ?
+  let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in import nixpkgs { }}: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+    nativeBuildInputs = with pkgs; [ nix home-manager git neovim ];
+  };
+}
