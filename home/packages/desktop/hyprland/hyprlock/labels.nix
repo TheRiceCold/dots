62f35400c8909acb4af41c37bb4b6d62175fd303
kaizen-dw <dalewaltergh@gmail.com>
2024-03-05 23:46:26 +0800

    style: hyprlock

---
+++ home/packages/desktop/hyprland/hyprlock/labels.nix
@@ -0,0 +1,31 @@
+[
+  { # Time
+    monitor = "";
+    font_size = 128;
+    valign = "center";
+    halign = "center";
+    position = { x = 0; y = 16; };
+    font_family = "JetBrainsMono Nerd Font";
+    text = ''cmd[update:1000] echo "<b>$(date +"%I:%M%p")</b>"'';
+  }
+
+  { # User
+    monitor = "";
+    font_size = 20;
+    halign = "center";
+    valign = "center";
+    position = { x = 0; y = 0; };
+    text = "How's like being a bum $USER?";
+    font_family = "JetBrainsMono Nerd Font";
+  }
+
+  { # Type to unlock
+    monitor = "";
+    font_size = 16;
+    halign = "center";
+    valign = "bottom";
+    position = { x = 0; y = 30; };
+    text = "Hey nerd, type to unlock";
+    font_family = "JetBrainsMono Nerd Font";
+  }
+]
