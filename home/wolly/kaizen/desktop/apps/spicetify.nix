6898199d2b01d4df042bf18818b39464b21b0ceb
TheRiceCold <dalewaltergh@gmail.com>
2024-08-16 23:55:09 +0800

    chore: spicetify theme and extensions

--- home/wolly/kaizen/desktop/apps/spicetify.nix
+++ home/wolly/kaizen/desktop/apps/spicetify.nix
@@ -3,7 +3,20 @@ let
   inherit (inputs) spicetify-nix;
 in {
   imports = [ spicetify-nix.homeManagerModules.default ];
-  programs.spicetify = {
+  programs.spicetify = let
+   spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
+  in
+  {
     enable = true;
+    theme = spicePkgs.themes.starryNight;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      powerBar
+      trashbin
+      loopyLoop
+      popupLyrics
+      playlistIcons
+      keyboardShortcut
+    ];
   };
 }
